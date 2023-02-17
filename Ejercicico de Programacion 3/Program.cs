8a24b7442458a0da14528d347c40e1223ff7c70f
Brandón <Brandn@LAPTOP-K2SR54QM>
2023-02-17 16:42:52 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicico de Programacion 3/Program.cs
@@ -0,0 +1,17 @@
+﻿long num1, num2;
+long suma;
+long resta;
+long producto;
+
+Console.WriteLine("Escriba su primer numero:");
+num1 = long.Parse(Console.ReadLine());
+Console.WriteLine("Escriba su segundo numero:");
+num2 = long.Parse(Console.ReadLine());
+suma = num1 + num2;
+resta = num1 - num2;
+producto = num1 * num2;
+Console.WriteLine("El resultado de la suma es:" + suma);
+Console.WriteLine("El resultado de la resta es:" + resta);
+Console.WriteLine("El resultado del Producto es:" + producto);
+
+
